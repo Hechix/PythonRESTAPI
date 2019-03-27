b75a32b141318e4cbd21bd3ede203d016e492e39
Hechix <jatonidonet@hotmail.com>
2019-03-27 19:48:31 +0100

    Optimizacion y cambios de nombre

--- principal.py
+++ principal.py
@@ -1,35 +1,45 @@
-import socket
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from threading import Thread
 
 
-def peticion(cliente, direccion):
-    print(direccion)
-    http = cliente.recv(1024)
+def leerConfiguracion():
+    print("//TODO")#Cambiar a loggin
+    #TODO EN #2
+
+
+def peticion(cliente_conexion, cliente_direccion):
+    #TODO EN #3
+    print(cliente_direccion)
+    http = cliente_conexion.recv(1024)
     print(http)
-    cliente.sendall(http)
-    cliente.close()
+    cliente_conexion.sendall(http)
+    cliente_conexion.close()
+
 
 def main():
-    print("Iniciando Server")
-    HOST, PORT = '', 80
+    print("Iniciando Server") #Cambiar a loggin
+    leerConfiguracion()
+    servidor, puerto = '', 80 #Leer esto de configuracion
 
-    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    listen_socket.bind((HOST, PORT))
-    listen_socket.listen(10)
+    servidor_escucha = socket(AF_INET, SOCK_STREAM)
+    servidor_escucha.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    servidor_escucha.bind((servidor, puerto))
+    servidor_escucha.listen(10)
 
-    print("Server iniciado")
+    print("Server iniciado")#Cambiar a loggin
 
     while True:
         try:
-            client_connection, client_address = listen_socket.accept()
-            print(client_address,"Conexion entrante")
-            
-            peti = Thread(target=peticion, args=(client_connection, client_address,))
+            cliente_conexion, cliente_direccion = servidor_escucha.accept()
+            print(cliente_direccion, "Conexion entrante")#Cambiar a loggin
+
+            peti = Thread(target=peticion, args=(
+                cliente_conexion, cliente_direccion,))
             peti.start()
-            
+
         except Exception as e:
-            print("/!\\ ERROR:", e)
+            print("/!\\ ERROR:", e)#Cambiar a loggin
+
 
 if __name__ == '__main__':
     main()
