7e2031f87de30d3a91f3c7d5171c1c57809c43a3
Hechix <jatonidonet@hotmail.com>
2019-03-27 20:19:35 +0100

    Formateo

--- principal.py
+++ principal.py
@@ -4,25 +4,26 @@ import logging
 
 
 def leerConfiguracion():
-    print("//TODO")#Cambiar a loggin
-    #TODO EN #2
+    print("//TODO")  # Cambiar a loggin
+    # TODO EN #2
 
 
 def peticion(cliente_conexion, cliente_direccion):
     global SERVIDOR_ENCENDIDO
-    #TODO EN #3
+    # TODO EN #3
     print(cliente_direccion)
     http = cliente_conexion.recv(1024)
     print(http)
     cliente_conexion.sendall(http)
     cliente_conexion.close()
 
+
 def main():
-    global SERVIDOR_ENCENDIDO,SERVIDOR_ENLACE
+    global SERVIDOR_ENCENDIDO, SERVIDOR_ENLACE
     leerConfiguracion()
-    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO, datefmt='%d-%m-%y %H:%M:%S')
+    logging.basicConfig(format='%(asctime)s - %(message)s',
+                        level=logging.INFO, datefmt='%d-%m-%y %H:%M:%S')
     logging.debug('INICIANDO SERVER')
-    servidor, puerto = '', 80 
     servidor_escucha = socket(AF_INET, SOCK_STREAM)
     servidor_escucha.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     servidor_escucha.bind(SERVIDOR_ENLACE)
@@ -37,10 +38,10 @@ def main():
             peti.start()
 
         except Exception as e:
-            logging.error("/!\\ ERROR - "+ str(e))
+            logging.error("/!\\ ERROR - " + str(e))
 
 
 if __name__ == '__main__':
     SERVIDOR_ENCENDIDO = True
-    SERVIDOR_ENLACE = ('',-1)#Modificar esto de configuracion
+    SERVIDOR_ENLACE = ('', -1)  # Modificar esto de configuracion
     main()
