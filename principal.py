4dbf40d09c70aef03bf667b4b3210b9e26a476a2
Hechix <jatonidonet@hotmail.com>
2019-03-27 21:44:46 +0100

    Funcionlidad del registro

--- principal.py
+++ principal.py
@@ -15,15 +15,12 @@ def peticion(cliente_conexion, cliente_direccion):
 
 def main():
     global CONFIGURACION
-    logging.basicConfig(format='%(asctime)s - %(message)s',
-                        level=logging.INFO, datefmt='%d-%m-%y %H:%M:%S')
     cargar_configuracion()
     logging.debug('INICIANDO SERVER')
     servidor_escucha = socket(AF_INET, SOCK_STREAM)
     servidor_escucha.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     servidor_escucha.bind(CONFIGURACION['SERVIDOR_ENLACE'])
     servidor_escucha.listen(10)
-    
     while CONFIGURACION['SERVIDOR_ENCENDIDO']:
         try:
             cliente_conexion, cliente_direccion = servidor_escucha.accept()
@@ -40,7 +37,6 @@ def cargar_configuracion():
     with open("configuracion.conf", "r") as archivo_configuracion:
         try:
             for linea in archivo_configuracion.readlines():
-
                 # Eliminar los comentarios
                 linea = linea.split("#")[0].strip()
 
@@ -59,34 +55,65 @@ def cargar_configuracion():
 
                     else:
                         if parametro in CONFIGURACION.keys():
-                            CONFIGURACION[parametro] = valor
+                            if valor == 'True' or valor == 'False':
+                                CONFIGURACION[parametro] = eval(valor)
+
+                            else:
+                                CONFIGURACION[parametro] = valor
+                                
                         else:
-                            logging.error(
-                                'AVISO: PARAMETRO NO RECONOCIDO\t'+parametro+' = '+valor)
+                            logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                                                level=logging.INFO, datefmt='%d-%m-%y %H:%M:%S')
+                            logging.warn(
+                                'PARAMETRO NO RECONOCIDO  -> '+parametro+' = '+valor)
 
         except Exception as e:
+            logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                                level=logging.INFO, datefmt='%d-%m-%y %H:%M:%S')
             logging.error('PARAMETRO INVALIDO:\t' + parametro +
                           ' = ' + valor + '\n\nDetalle:\t' + str(e)+"\n\nSi el problema persiste, por favor informa:\n\thttps://github.com/Hechix/PythonRESTAPI/issues")
             exit()
 
-    if CONFIGURACION['LOG_DEBUG']:
-        logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                            level=logging.DEBUG, datefmt='%d-%m-%y %H:%M:%S')
-        logging.debug('Configuracion:\n')
+    if CONFIGURACION['REGISTRO_IGNORAR']:
+        print('Bienvenido a Hechix\'s Python REST API\nEl registo está deshabilitado, puedes activarlo en configuracion.conf')
 
-        for parametro in CONFIGURACION.keys():
-            logging.debug(parametro + (' ' * (25 - len(parametro))
-                                       ) + str(CONFIGURACION[parametro]))
+    else:
+        if CONFIGURACION['REGISTRO_DEBUG']:
+            if CONFIGURACION['REGISTRO_ALMACENAR']:
+                print('Bienvenido a Hechix\'s Python REST API\nEl registo está siendo almacenado y no aparecerá en consola, puedes cambiarlo en configuracion.conf')
+                logging.basicConfig(filename='Registro.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s',
+                                    level=logging.DEBUG, datefmt='%d-%m-%y %H:%M:%S')
 
-        logging.debug(('-' * 30)+'\n')
+            else:
+                logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                                    level=logging.DEBUG, datefmt='%d-%m-%y %H:%M:%S')
+
+            logging.debug('Configuracion:\n')
+
+            for parametro in CONFIGURACION.keys():
+                logging.debug(parametro + (' ' * (25 - len(parametro))
+                                           ) + str(CONFIGURACION[parametro]))
+
+            logging.debug(('-' * 30)+'\n')
+
+        else:
+            if CONFIGURACION['REGISTRO_ALMACENAR']:
+                print('Bienvenido a Hechix\'s Python REST API\nEl registo está siendo almacenado y no aparecerá en consola, puedes cambiarlo en configuracion.conf')
+                logging.basicConfig(filename='Registro.log', filemode='a', format='%(asctime)s - %(message)s',
+                                    level=logging.INFO, datefmt='%d-%m-%y %H:%M:%S')
+
+            else:
+                logging.basicConfig(format='%(asctime)s - %(message)s',
+                                    level=logging.INFO, datefmt='%d-%m-%y %H:%M:%S')
 
 
 if __name__ == '__main__':
     CONFIGURACION = {
         'SERVIDOR_ENCENDIDO': True,
         'SERVIDOR_ENLACE': ('', -1),
-        'LOG_DEBUG': True,
-        'LOG_ALMACENAR': False,
+        'REGISTRO_IGNORAR': False,
+        'REGISTRO_DEBUG': True,
+        'REGISTRO_ALMACENAR': False,
         'TIPO_SERVER': 'JSON',
         'ACEPTAR_GET': True,
         'ACEPTAR_POST':  True,
