6c4087e4797e59cc3fae9737190f1aed075c2da2
Hechix <jatonidonet@hotmail.com>
2019-04-09 22:57:38 +0200

    Funcional, falta revisar

--- Peticion.py
+++ Peticion.py
@@ -171,5 +171,28 @@ class Peticion:
         raise Exception('PUT no implementado')
 
     def DELETE(self):
-        self.devolver_estado(400)
+
+        #TODO #22 MOVER ESTO A ANTES DE LLAMAR LA FUNCION
+        self.logging.info(self.cliente_direccion + ' DELETE ' + self.URI)
+        trozos_URI = self.URI.split('?')
+
+        if len(trozos_URI) > 1:
+            self.devolver_estado(400)
+
+        trozos_URI = trozos_URI[0].split("/") 
+        # El 1º indice es '', puede que existan otros si se introduce en la URL AAAA//BBBB en vez de AAAA/BBBB (repeticiones de / sin nada en medio) o similares
+        trozos_URI = [ elemento_en_URI for elemento_en_URI in trozos_URI if elemento_en_URI != '']
+
+        if not len(trozos_URI) == 2: 
+            self.devolver_estado(400)
+
+        json = almacenamiento.cargar_json(self.CONFIGURACION)
+        encontrado, objeto_encontrado = almacenamiento.buscar_objeto(self.CONFIGURACION, trozos_URI, json)
+        
+        if encontrado :
+            almacenamiento.borrar_objeto(self.CONFIGURACION,trozos_URI,json, objeto_encontrado)
+            self.devolver_estado(200)
+        else:
+            self.devolver_estado(404)
+            
 
